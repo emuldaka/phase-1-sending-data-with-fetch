1c1427afadf5e6066df7ab22df8cfd221453ba92
Elis <emuldaka@gmail.com>
2023-02-15 13:32:24 -0600

    1

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 // Add your code here
-const formData = {};
-
-const configurationObject = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  body: JSON.stringify(formData),
+const formData = {
+  name: "",
+  email: "",
 };
 
 function submitData(name, email) {
   formData.name = name;
   formData.email = email;
-  console.log("picnic", formData);
+  console.log("picnic", JSON.stringify(formData));
+
+  let configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
   return fetch("http://localhost:3000/users", configurationObject)
     .then(function (response) {
       return response.json();
     })
-    .then(function (object) {
-      console.log(object);
+    .then(function (data) {
+      const newId = data.id;
+      const idElement = document.createElement("p");
+      idElement.textContent = "New ID: " + newId;
+      document.body.appendChild(idElement);
     })
     .catch(function (error) {
-      alert("Bad things! Ragnarők!");
-      console.log(error.message);
+      const message = error.message;
+      const idElement = document.createElement("p");
+      idElement.textContent = message;
+      document.body.appendChild(idElement);
     });
 }
